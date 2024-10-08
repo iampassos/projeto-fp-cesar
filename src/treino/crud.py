5b00e19e9d5533e056127c6a87a2e9abcde1afa4
iampassos <78429887+iampassos@users.noreply.github.com>
2024-10-08 20:26:37 -0300

    Todas as funcionalidades iniciais

--- src/treino/crud.py
+++ src/treino/crud.py
@@ -1,9 +1,7 @@
-import ferramentas
+from .. import ferramentas
 
-treinoCaminho = "./data/treinos.csv"
 
-# Adicionar um treino/competição ao banco de dados
-# Formato: (A ordem importa)
+# Formato geral de respostas: (A ordem importa)
 # {
 #     "tipo": "",
 #     "data": "",
@@ -12,6 +10,12 @@ treinoCaminho = "./data/treinos.csv"
 #     "localizacao": "",
 #     "clima": "",
 # }
+# Em algumas funções, será retornado uma LISTA ou None
+
+treinoCaminho = "./data/treinos.csv"
+
+
+# Adicionar um treino/competição ao banco de dados
 
 
 def addTreino(dados):
@@ -22,6 +26,57 @@ def addTreino(dados):
 
     novoTreino = ",".join(str(valor) for valor in dados.values()).split(",")
     arquivo.append(novoTreino)
+
+    ferramentas.escreverCSV(treinoCaminho, arquivo)
+
+
+# Ler um treino/competição do banco de dados baseado no índice (começa no 1)
+
+
+def lerTreino(index=None):
+    arquivo = ferramentas.lerCSV(treinoCaminho)
+
+    if not index:
+        return arquivo
+
+    if index >= len(arquivo) or index < 0:
+        return None
+
+    return arquivo[index]
+
+
+# Atualizar um treino/competição do banco de dados
+# a partir do índice (começa no 1)
+# Obs: dados são uma LISTA com os dados em ordem
+# atualizarTreino(0, ["", "", etc..])
+
+
+def atualizarTreino(index, dados):
+    treino = lerTreino(index)
+
+    if not treino:
+        return None
+
+    arquivo = ferramentas.lerCSV(treinoCaminho)
+    arquivo[index] = dados
+
+    ferramentas.escreverCSV(treinoCaminho, arquivo)
+
+    return dados
+
+
+# Remover um treino/competição do banco de dados
+# baseado no índice (começa no 1)
+
+
+def removerTreino(index):
+    treino = lerTreino(index)
+
+    if not treino:
+        return
+
+    arquivo = ferramentas.lerCSV(treinoCaminho)
+    arquivo.pop(index)
 
     ferramentas.escreverCSV(treinoCaminho, arquivo)
 
