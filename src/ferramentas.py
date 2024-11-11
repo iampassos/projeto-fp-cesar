7388033693b05f9277074ea0058df7b2c7395cb3
iampassos <78429887+iampassos@users.noreply.github.com>
2024-11-11 17:01:48 -0300

    docs: add documentação

--- src/ferramentas.py
+++ src/ferramentas.py
@@ -1,14 +1,21 @@
 import os
 
-"""
-Ler de arquivo .csv
-Essa função vai tratar os dados do arquivo .csv e vai retornar uma lista
-com todos os dados. Ela cria um novo arquivo caso ele não exista
-Ex: [["nome", "idade"], ["João", 17], ["Mircio", 18]]
-"""
-
 
 def ler_csv(caminho):
+    """
+    Lê dados de um arquivo CSV.
+
+    Esta função trata os dados de um arquivo CSV e retorna uma lista
+    contendo todos os dados. Caso o arquivo não exista, ele é criado
+    automaticamente como um arquivo vazio.
+
+    Args:
+        caminho (str): Caminho do arquivo CSV a ser lido.
+
+    Returns:
+        list: Lista de listas contendo os dados do CSV.
+              Exemplo: [["nome", "idade"], ["João", 17], ["Mircio", 18]]
+    """
     if not os.path.exists(caminho):
         escrever_csv(caminho, [])
 
@@ -22,15 +29,19 @@ def ler_csv(caminho):
     return dados
 
 
-"""
-Escrever para arquivo .csv
-Essa função vai escrever para o arquivo .csv no formato que ele precisa.
-Aqui você pode reescrever o banco de dados inteiro, então cuidado
-Ex: [["nome", "idade"], ["João", 17], ["Mircio", 18]]
-"""
-
-
 def escrever_csv(caminho, dados):
+    """
+    Escreve dados em um arquivo CSV.
+
+    Esta função escreve a lista de dados fornecida para o arquivo CSV.
+    É importante lembrar que esta função reescreve todo o conteúdo do arquivo,
+    então use-a com cuidado para evitar a perda de dados.
+
+    Args:
+        caminho (str): Caminho do arquivo CSV onde os dados serão escritos.
+        dados (list): Lista de listas contendo os dados a serem gravados no CSV
+                    Exemplo: [["nome", "idade"], ["João", 17], ["Mircio", 18]]
+    """
     with open(caminho, "w", encoding="utf8") as arquivo:
         for formato in dados:
             linha = ",".join(str(valor) for valor in formato)
