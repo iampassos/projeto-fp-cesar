e0c2cd155096ab5609c1fb9d1b076d36091cc651
Mircio-Cesar <mfsn@cesar.school>
2024-11-11 10:06:52 -0300

    adicionei encoding

--- src/ferramentas.py
+++ src/ferramentas.py
@@ -10,7 +10,7 @@ def ler_csv(caminho):
     if not os.path.exists(caminho):
         escrever_csv(caminho, [])
 
-    with open(caminho, "r") as arquivo:
+    with open(caminho, "r",encoding="utf8") as arquivo:
         dados = []
 
         for linha in arquivo.readlines():
@@ -26,7 +26,7 @@ def ler_csv(caminho):
 # Ex: [["nome", "idade"], ["João", 17], ["Mircio", 18]]
 
 def escrever_csv(caminho, dados):
-    with open(caminho, "w") as arquivo:
+    with open(caminho, "w", encoding="utf8") as arquivo:
         for formato in dados:
             linha = ",".join(str(valor) for valor in formato)
             arquivo.write(f"{linha}\n")
