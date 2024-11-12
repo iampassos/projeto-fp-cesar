1ac1d6174e545560ea8675354623b92d033dd881
iampassos <78429887+iampassos@users.noreply.github.com>
2024-11-12 10:23:01 -0300

    feat: add acompanhamento metas

--- src/metas/funcionalidades.py
+++ src/metas/funcionalidades.py
@@ -1,28 +1,52 @@
+from datetime import datetime
 from ..registros import registro as Registro
+from . import meta as Meta
 
 
-def distancia_mes(dados: list[Registro.Registro], mes: int):
+def registros_mes(dados: list[Registro.Registro], mes: int, ano: int):
     filtrados = []
 
     for i in dados:
-        print(i)
-        if i.data.mes == mes:
+        if i.data.mes == mes and i.data.ano == ano:
             filtrados.append(i)
 
     return filtrados
 
 
-distancia_mes(
+def acompanhar_meta_distancia(dados: list[Registro.Registro], meta: Meta.Meta):
+    filtrados = registros_mes(dados, datetime.now().month)
+
+    soma = 0
+
+    for i in filtrados:
+        soma += i.distancia
+
+    return soma
+
+
+def acompanhar_meta_tempo(dados: list[Registro.Registro], meta: Meta.Meta):
+    filtrados = registros_mes(
+        dados, datetime.now().month, datetime.now().year % 100)
+
+    resultados = []
+
+    for i in filtrados:
+        tempo = (i.distancia / i.duracao) * 60
+        resultados.append(tempo >= (meta.quantidade / meta.tempo) * 60)
+
+    return resultados
+
+
+print(acompanhar_meta_tempo(
     [
-        Registro.Registro("treino", "12/11/24", 100,
-                          100, "recife", "agradavel"),
-        Registro.Registro("treino", "12/11/24", 100,
-                          100, "recife", "agradavel"),
-        Registro.Registro("treino", "12/11/24", 100,
-                          100, "recife", "agradavel"),
-        Registro.Registro("treino", "12/11/24", 100,
-                          100, "recife", "agradavel"),
-        Registro.Registro("treino", "12/11/24", 100,
-                          100, "recife", "agradavel"),
-    ]
-)
+        Registro.Registro("treino", "12/11/21", 80,
+                          20, "recife", "agradavel"),
+        Registro.Registro("treino", "12/11/21", 2,
+                          120, "recife", "agradavel"),
+        Registro.Registro("treino", "12/11/22", 10,
+                          60, "recife", "agradavel"),
+        Registro.Registro("treino", "12/10/22", 4.9,
+                          60, "recife", "agradavel"),
+    ],
+    Meta.Meta("tempo", "Melhorar tempo em 5km/h", 5, 60)
+))
--- src/metas/meta.py
+++ src/metas/meta.py
@@ -1,8 +1,10 @@
 class Meta:
-    def __init__(self, tipo: str, descricao: str, quantidade: int) -> None:
+    def __init__(self, tipo: str, descricao: str, quantidade: int,
+                 tempo=None) -> None:
         self.tipo = tipo
         self.descricao = descricao
         self.quantidade = quantidade
+        self.tempo = tempo
 
     def __str__(self):
         return f"{self.tipo}, {self.descricao}, {self.quantidade}"
